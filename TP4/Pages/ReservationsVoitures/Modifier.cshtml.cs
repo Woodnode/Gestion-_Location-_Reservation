5dd67175d861fc25b86896d5af2e90adf2ae4cc0
Woodnode <barret.julien10@gmail.com>
2025-04-15 12:21:35 -0400

    V1 pages Modifier et Supprimer.

--- TP4/Pages/ReservationsVoitures/Modifier.cshtml.cs
+++ TP4/Pages/ReservationsVoitures/Modifier.cshtml.cs
@@ -1,12 +1,68 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TP4.Services;
+using TP4.Models;
 
 namespace TP4.Pages.ReservationsVoitures
 {
     public class ModifierModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public required Reservation Reservation { get; set; }
+
+        public required List<Voiture> ListeDeVoitures { get; set; }
+
+        [BindProperty]
+        public int IdVoiture { get; set; }
+
+        public ActionResult OnGet(int? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (GestionReservable.ObtenirReservableParId("Reservation", id.Value) is not Reservation reservation || reservation is null)
+            {
+                return NotFound();
+            }
+
+            if (reservation.ObjetDeLaReservation is null || GestionReservable.ObtenirReservableParId("Voiture", reservation.ObjetDeLaReservation.Id) is not Voiture voiture)
+            {
+                return NotFound();
+            }
+
+            Reservation = reservation;
+            ChargementDePage();
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                ChargementDePage();
+                return Page();
+            }
+
+            if (Reservation.DateFin < DateTime.Now.Date)
+            {
+                ModelState.AddModelError(string.Empty, "Vous ne pouvez pas modifier une réservation passée ou en cours.");
+                ChargementDePage();
+                return Page();
+            }
+
+            GestionReservable.ModifierReservable(Reservation);
+            return RedirectToPage("Index");
+        }
+
+        private void ChargementDePage()
+        {
+            ListeDeVoitures = GestionReservable.ObtenirListeReservable("Voiture")
+                .Cast<Voiture>()
+                .ToList();
+
+            IdVoiture = Reservation.ObjetDeLaReservation.Id;
         }
     }
 }
--- TP4/Pages/ReservationsVoitures/Supprimer.cshtml.cs
+++ TP4/Pages/ReservationsVoitures/Supprimer.cshtml.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TP4.Models;
+using TP4.Services;
 
 namespace TP4.Pages.ReservationsVoitures
 {
     public class SupprimerModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public required Reservation Reservation { get; set; }
+
+        public required Voiture Voiture { get; set; }
+
+        public void OnGet(int id)
         {
+            ChargementDePage(id);
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Reservation.DateFin < DateTime.Now.Date)
+            {
+                ModelState.AddModelError("", "Vous ne pouvez pas supprimer une réservation passée ou en cours.");
+                ChargementDePage(Reservation.Id);
+                return Page();
+            }
+
+            GestionReservable.SupprimerReservable(Reservation);
+            return RedirectToPage("Index");
+        }
+
+        private void ChargementDePage(int id)
+        {
+            Reservation = GestionReservable.ObtenirReservableParId("Reservation", id) as Reservation;
+
+            if (Reservation == null)
+            {
+                RedirectToPage("Index");
+            }
+
+            Voiture = GestionReservable.ObtenirReservableParId("Voiture", Reservation.ObjetDeLaReservation.Id) as Voiture;
         }
     }
 }
